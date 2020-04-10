48e0e93f1a138de258de4ad3ecd917acc2f1d5a3
mahoyen <32592450+mahoyen@users.noreply.github.com>
2020-04-10 15:32:22 +0200

    added test for shiftBy

--- MP3/test_scrabble.py
+++ MP3/test_scrabble.py
@@ -6,27 +6,70 @@ import numpy.testing as nptest
 
 from scrabble import ScrabbleLetters
 # -
-
+# +
 testLettersDataFrame = pd.DataFrame()
 testLettersDataFrame["Letter"] = [" ", "I", "A"]
 testLettersDataFrame["Count"] = [2, 2, 5]
 testLettersDataFrame["Points"] = [0, 3, 5]
 
-
-def test_initalizeIntLettersArrayFromDataFrame():
-    scrbl_letters = ScrabbleLetters()
-    
-    intLetterArray = scrbl_letters.initalizeIntLettersArrayFromDataFrame(testLettersDataFrame)
-    correctIntLetterArray = np.array([[ord(" "), ord(" "), ord("A")],
+correctIntLetterArray = np.array([[ord(" "), ord(" "), ord("A")],
                                         [ord("A"), ord("A"), ord("A")],
                                         [ord("A"), ord("I"), ord("I")]])
-                                        
+
+        
+
+# -
+# +
+
+def test_initalizeIntLettersArrayFromDataFrame():
+    # Setup
+    scrbl_letters = ScrabbleLetters()
+    
+    # Test
+    intLetterArray = scrbl_letters.initalizeIntLettersArrayFromDataFrame(testLettersDataFrame)
+
+    # Compare                                 
     nptest.assert_array_equal(intLetterArray, correctIntLetterArray)
     
-    del scrbl_letters
 # -
+# +
+axis0shift000 =   np.array([[ord(" "), ord(" "), ord("A")],
+                            [ord("A"), ord("A"), ord("A")],
+                            [ord("A"), ord("I"), ord("I")]])
+                            
+axis0shift123 =   np.array([[ord("A"), ord(" "), ord(" ")],
+                            [ord("A"), ord("A"), ord("A")],
+                            [ord("A"), ord("I"), ord("I")]])
 
+axis1shift123 =   np.array([[ord("A"), ord("A"), ord("A")],
+                            [ord(" "), ord("I"), ord("A")],
+                            [ord("A"), ord(" "), ord("I")]])
 
+axis1shift222 =   np.array([[ord("A"), ord("A"), ord("A")],
+                            [ord("A"), ord("I"), ord("I")],
+                            [ord(" "), ord(" "), ord("A")]])
+
+axis0shift222 =   np.array([[ord(" "), ord("A"), ord(" ")],
+                            [ord("A"), ord("A"), ord("A")],
+                            [ord("I"), ord("I"), ord("A")]])
+# -
+# +
+@pytest.mark.parametrize("shifts,axis,expected", [([0,0,0], 0, axis0shift000),
+                                                  ([1,2,3], 0, axis0shift123),
+                                                  ([1,2,3], 1, axis1shift123),
+                                                  ([2,2,2], 1, axis1shift222),
+                                                  ([2,2,2], 0, axis0shift222),
+                                                  ])
+def test_shiftBy(shifts,axis,expected):
+    # Setup
+    scrbl_letters = ScrabbleLetters()
+    scrbl_letters.intLetterArray = correctIntLetterArray.copy()
+
+    # Test
+    scrbl_letters.shiftBy(shifts, axis=axis)
+
+    # Compare
+    nptest.assert_array_equal(scrbl_letters.intLetterArray, expected)
 
 
 
