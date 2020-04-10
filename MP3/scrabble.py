56ef861743823638ee17526cd7d72fee7dc552a3
mahoyen <32592450+mahoyen@users.noreply.github.com>
2020-04-10 14:07:29 +0200

    added shift and skip functions

--- MP3/scrabble.py
+++ MP3/scrabble.py
@@ -21,6 +21,52 @@ screen = ScrabbleScreen()
 screen.updateLetterArray(letterArray)
 screen.printScreen()
 
+screen.printScreen()
+
+
+def shiftBy(letterArray, shifts, axis=0):
+    if np.size(letterArray, axis=axis) != len(shifts):
+        raise ValueError("length of shifts does not equal length of array along axis")
+        return
+    
+    
+    transposedLetterArray = letterArray.transpose((int(1==axis), int(0==axis)))
+    
+    for iRow, shift in enumerate(shifts):
+        transposedLetterArray[iRow, :] = np.roll(transposedLetterArray[iRow, :], shift)
+        
+    letterArray = transposedLetterArray.transpose((int(1==axis), int(0==axis)))
+    
+
+def swap_every_n(letterArray, n, axis=0):
+    transposedLetterArray = letterArray.transpose((int(1==axis), int(0==axis)))
+    
+    nSwaps = len(transposedLetterArray)//n
+    for iSwap in range(nSwaps):
+        transposedLetterArray[:, [iSwap, iSwap+n]] = transposedLetterArray[:, [iSwap+n, iSwap]]
+        
+    
+    letterArray = transposedLetterArray.transpose((int(1==axis), int(0==axis)))
+        
+
+swap_every_n(letterArray, 2, axis=1)
+screen.printScreen()
+
+shiftBy(letterArray, [0,1,2,3,4,5,6,7,8,9], axis=1)
+shiftedletterArray
+
+screen.printScreen()
+
+screen.updateLetterArray(shiftedletterArray)
+screen.printScreen()
+
+screen.updateLetterArray(shiftedletterArray)
+screen.printScreen()
+
+axis = 0
+for letters in letterArray.transpose((int(1==axis), int(0==axis))):
+    print(letters)
+
 
 
 
