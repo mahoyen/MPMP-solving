e2c3e8bd6b4bc196b4127c747039770bf134b269
mahoyen <32592450+mahoyen@users.noreply.github.com>
2020-04-10 15:09:59 +0200

    created scrabbleClass

--- MP3/scrabble.py
+++ MP3/scrabble.py
@@ -2,71 +2,54 @@ import numpy as np
 from scrbl_interface import ScrabbleScreen
 import pandas as pd
 
-fileNameLetterCSV = "no_scrabble_letters.csv"
-letter_df = pd.read_csv(fileNameLetterCSV)
-letter_df.sort_values("Letter", inplace=True)
-letter_df.head()
 
-# +
-letterList = []
-for letter, count in zip(letter_df["Letter"], letter_df["Count"]):
-    letterList += [ord(letter)] * count
+class ScrabbleLetters:
 
-letterArray = np.array(letterList)
-letterArray = np.reshape(letterArray,(10,10))
-letterArray
-# -
-
-screen = ScrabbleScreen()
-screen.updateLetterArray(letterArray)
-screen.printScreen()
-
-screen.printScreen()
-
-
-def shiftBy(letterArray, shifts, axis=0):
-    if np.size(letterArray, axis=axis) != len(shifts):
-        raise ValueError("length of shifts does not equal length of array along axis")
-        return
+    def __init__(self, fileNameLetterCSV=None):
+        if fileNameLetterCSV is not None:
+            self.letter_df = loadLettersDataFrameFromFile(fileNameLetterCSV)
+            self.intLetterArray = initalizeIntLettersArrayFromDataFrame(self.letter_df)
+            self.screen = ScrabbleScreen(self.intLetterArray)
     
-    
-    transposedLetterArray = letterArray.transpose((int(1==axis), int(0==axis)))
-    
-    for iRow, shift in enumerate(shifts):
-        transposedLetterArray[iRow, :] = np.roll(transposedLetterArray[iRow, :], shift)
+    def loadLettersDataFrameFromFile(self, fileNameLetterCV):
+        letter_df = pd.read_csv(fileNameLetterCSV)
+        return letter_df
         
-    letterArray = transposedLetterArray.transpose((int(1==axis), int(0==axis)))
-    
+    def initalizeIntLettersArrayFromDataFrame(self, letter_df):
+        letter_df.sort_values("Letter", inplace=True)
+        intLetterList = []
+        for letter, count in zip(letter_df["Letter"], letter_df["Count"]):
+            intLetterList += [ord(letter)] * count
 
-def swap_every_n(letterArray, n, axis=0):
-    transposedLetterArray = letterArray.transpose((int(1==axis), int(0==axis)))
+        nRow = int(np.ceil(np.sqrt(len(intLetterList))))
+        nCols = int(np.ceil(len(intLetterList)/nRow))
+
+        intLetterArray = np.array(intLetterList)
+        intLetterArray = np.reshape(intLetterArray,(nRow,nCols))
+        return intLetterArray
     
-    nSwaps = len(transposedLetterArray)//n
-    for iSwap in range(nSwaps):
-        transposedLetterArray[:, [iSwap, iSwap+n]] = transposedLetterArray[:, [iSwap+n, iSwap]]
+    def showScreen():
+        screen.printScreen()
         
-    
-    letterArray = transposedLetterArray.transpose((int(1==axis), int(0==axis)))
+    def shiftBy(self, shifts, axis=0):
+        if np.size(self.letterArray, axis=axis) != len(shifts):
+            raise ValueError("length of shifts does not equal length of array along axis")
+            return
+
+
+        transposedLetterArray = self.letterArray.transpose((int(1==axis), int(0==axis)))
+
+        for iRow, shift in enumerate(shifts):
+            transposedLetterArray[iRow, :] = np.roll(transposedLetterArray[iRow, :], shift)
+
+        self.letterArray = transposedLetterArray.transpose((int(1==axis), int(0==axis)))
         
+    def swapEveryN(self, n, axis=0):
+        transposedLetterArray = self.letterArray.transpose((int(1==axis), int(0==axis)))
 
-swap_every_n(letterArray, 2, axis=1)
-screen.printScreen()
-
-shiftBy(letterArray, [0,1,2,3,4,5,6,7,8,9], axis=1)
-shiftedletterArray
-
-screen.printScreen()
-
-screen.updateLetterArray(shiftedletterArray)
-screen.printScreen()
-
-screen.updateLetterArray(shiftedletterArray)
-screen.printScreen()
-
-axis = 0
-for letters in letterArray.transpose((int(1==axis), int(0==axis))):
-    print(letters)
+        nSwaps = len(transposedLetterArray)//n
+        for iSwap in range(nSwaps):
+            transposedLetterArray[:, [iSwap, iSwap+n]] = transposedLetterArray[:, [iSwap+n, iSwap]]
 
 
-
-
+        self.letterArray = transposedLetterArray.transpose((int(1==axis), int(0==axis)))
